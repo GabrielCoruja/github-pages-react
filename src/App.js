e15fc53574702f8517d1ace2eb0f4b620c2e9617
Gabriel Dalseco <gabriel_santos96@outlook.com>
2021-04-27 00:56:49 -0300

    mais uma

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import Soccer from './pages/Soccer';
 
 function App() {
   return (
-    <div>
-    <h1>Criando nossas rotas</h1>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/soccer" component={Soccer} />
       <Route exact path="/class" component={Class} />
     </Switch>
-    </div>
   );
 }
 
